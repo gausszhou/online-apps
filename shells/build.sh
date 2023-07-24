7db26ae27814cdfc56d71606adf56c45cea825f1
gausszhou <gausszhou@qq.com>
2023-07-25 00:23:49 +0800

    update deploy.sh

--- shells/build.sh
+++ shells/build.sh
@@ -7,3 +7,6 @@ echo "pnpm success in $time"
 pnpm --filter @gausszhou/todos build
 time=$(date "+%Y-%m-%d %H:%M:%S")
 echo "pnpm success in $time"
+
+touch ./dist/README.md
+echo "# Apps" > ./dist/README.md
